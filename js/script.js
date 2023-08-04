1d771212c1dae8e1c506a866f9bcd0dc04e1e2a1
futuremike1 <73429447+futuremike1@users.noreply.github.com>
2023-08-04 16:14:35 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -15,18 +15,18 @@ $(function() { // Same as document.addEventListener("DOMContentLoaded"...
 
     var homeHtmlUrl = "snippets/home-snippet.html";
     var allCategoriesUrl =
-        "https://davids-restaurant.herokuapp.com/categories.json";
+      "https://coursera-jhu-default-rtdb.firebaseio.com/categories.json";
     var categoriesTitleHtml = "snippets/categories-title-snippet.html";
     var categoryHtml = "snippets/category-snippet.html";
     var menuItemsUrl =
-        "https://davids-restaurant.herokuapp.com/menu_items.json?category=";
+      "https://coursera-jhu-default-rtdb.firebaseio.com/menu_items/";
     var menuItemsTitleHtml = "snippets/menu-items-title.html";
     var menuItemHtml = "snippets/menu-item.html";
-
+    
     // Convenience function for inserting innerHTML for 'select'
-    var insertHtml = function(selector, html) {
-        var targetElem = document.querySelector(selector);
-        targetElem.innerHTML = html;
+    var insertHtml = function (selector, html) {
+      var targetElem = document.querySelector(selector);
+      targetElem.innerHTML = html;
     };
 
     // Show loading icon inside element identified by 'selector'.
